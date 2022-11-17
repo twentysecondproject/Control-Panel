9c544cdda195159d88af13688ce78a339b18c493
FILIPPO GENONI <filippo.genoni@studenti.collegiovilloresi.it>
2022-11-17 16:40:15 +0100

    Fixed some little things not meant for public consumption

--- cp/settings.py
+++ cp/settings.py
@@ -17,19 +17,19 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATE_INPUT_FORMATS = ('%d-%m-%Y')
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7eucsa%s2ykx45nltr%_67+l3_dt5p!@8mbfv^%%y%5l-rlckc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
